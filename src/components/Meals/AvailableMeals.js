01329c23a8d4cf2ef095acb87715171e71ac9984
rehagursel <92670670+rehagursel@users.noreply.github.com>
2022-03-24 21:53:52 +0300

    meal list added

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,10 +1,11 @@
 import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
   {
     id: "m1",
-    name: "Aegean appetizers with olive oil",
+    name: "Aegean Appetizers With Olive Oil",
     description: "Healthy...and green...",
     price: 4.99,
   },
@@ -35,7 +36,14 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,23 @@
+import classes from "./MealItem.module.css";
+
+const MealItem = (props) => {
+  const price = `$${props.price}`;
+  return (
+    <li>
+      <div className={classes.meal}>
+        <div className={classes.mealOrder}>
+          <div>
+            <h3>{props.name}</h3>
+            <div className={classes.price}>{price}</div>
+          </div>
+          <div>
+            <button>+ Add</button>
+          </div>
+        </div>
+        <div className={classes.description}>{props.description}</div>
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
